7af1516302a6399a3c703bc7846eef610084f6b7
hendyardhana <hendy.ardha1708@gmail.com>
2023-03-21 16:53:35 +0700

    Week4

--- app/src/main/java/com/example/week4_160420138/view/MainActivity.kt
+++ app/src/main/java/com/example/week4_160420138/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.week4_160420138
+package com.example.week4_160420138.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.week4_160420138.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
